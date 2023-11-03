2f94b86dcb48516c8f8bad2f125b8cc3460a51e5
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2023-11-02 20:41:01 -0400

    Function to render parties

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-const parties= [
+// Initial list
+const parties = [
     {id: 1, name: "Emma Lopez", description: "Wedding Reception", date: "2023-09-30T18:00:00.000Z", location: "5015 Fort Hamer Rd, Parrish, FL 34219"},
     {id: 2, name: "Mia Jones", description: "Birthday Party", date: "2023-09-30T15:00:00.000Z", location: "1339 E Fletcher Ave, Tampa, FL 33612"},
     {id: 3, name: "Liam Parker", description: "Halloween Party", date: "2023-10-31T19:30:00.000Z", location: "6152 126th Ave N #504, Largo, FL 33773"},
@@ -7,4 +8,22 @@ const parties= [
     {id: 6, name: "Sara Lee", description: "Fundraiser", date: "2023-11-18T11:00:00.000Z", location: "6463 W Hillsborough Ave, Tampa, FL 33634"},
     {id: 7, name: "Justin Brooks", description: "Birthday Party", date: "2023-11-25T16:00:00.000Z", location: "1339 E Fletcher Ave, Tampa, FL 33612"},
     {id: 8, name: "Amy Carter", description: "Graduation Party", date: "2023-12-02T17:30:00.000Z", location: "6152 126th Ave N #504, Largo, FL 33773"},
-]
+];
+
+// Function to render parties
+function renderParties() {
+    const partyList = document.getElementById('party-list');
+    partyList.innerHTML = '';
+
+    parties.forEach(party => {
+        const li = document.createElement('li');
+        li.innerHTML = `
+            <span>Name: ${party.name}</span>
+            <span>Date: ${new Date(party.date).toLocaleString()}</span>
+            <span>Location: ${party.location}</span>
+            <span>Description: ${party.description}</span>
+            <button class="delete-button" data-id="${party.id}">Delete</button>
+        `;
+        partyList.appendChild(li);
+    });
+}
