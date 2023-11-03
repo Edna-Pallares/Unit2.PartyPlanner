79311b429d7bf623346278d3c393df581e9d6c7b
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2023-11-02 23:54:18 -0400

    changes to HTML

--- index.js
+++ index.js
@@ -1,29 +1,10 @@
-// Initial list
-const parties = [
-    {id: 1, name: "Emma Lopez", description: "Wedding Reception", date: "2023-09-30T18:00:00.000Z", location: "5015 Fort Hamer Rd, Parrish, FL 34219"},
-    {id: 2, name: "Mia Jones", description: "Birthday Party", date: "2023-09-30T15:00:00.000Z", location: "1339 E Fletcher Ave, Tampa, FL 33612"},
-    {id: 3, name: "Liam Parker", description: "Halloween Party", date: "2023-10-31T19:30:00.000Z", location: "6152 126th Ave N #504, Largo, FL 33773"},
-    {id: 4, name: "Jack Miller", description: "Pool Party", date: "2023-11-04T16:00:00.000Z", location: "1701 Collins Ave, Miami Beach, FL 33139"},
-    {id: 5, name: "Luca Adams", description: "Wedding Reception", date: "2023-11-11T18:00:00.000Z", location: "5015 Fort Hamer Rd, Parrish, FL 34219"},
-    {id: 6, name: "Sara Lee", description: "Fundraiser", date: "2023-11-18T11:00:00.000Z", location: "6463 W Hillsborough Ave, Tampa, FL 33634"},
-    {id: 7, name: "Justin Brooks", description: "Birthday Party", date: "2023-11-25T16:00:00.000Z", location: "1339 E Fletcher Ave, Tampa, FL 33612"},
-    {id: 8, name: "Amy Carter", description: "Graduation Party", date: "2023-12-02T17:30:00.000Z", location: "6152 126th Ave N #504, Largo, FL 33773"},
-];
+const EVENTS_URI = "https://fsa-crud-2aa9294fe819.herokuapp.com/api/2308-ACC-PT-WEB-PT-A/events"
 
-// Function to render parties
-function renderParties() {
-    const partyList = document.getElementById('party-list');
-    partyList.innerHTML = '';
-
-    parties.forEach(party => {
-        const li = document.createElement('li');
-        li.innerHTML = `
-            <span>Name: ${party.name}</span>
-            <span>Date: ${new Date(party.date).toLocaleString()}</span>
-            <span>Location: ${party.location}</span>
-            <span>Description: ${party.description}</span>
-            <button class="delete-button" data-id="${party.id}">Delete</button>
-        `;
-        partyList.appendChild(li);
-    });
+const state = {
+    events: [],
 }
+
+const partiesList = document.querySelector("#display-parties");
+const form = document.querySelector("form");
+console.log(form);
+
