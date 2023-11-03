68288518796bd4fdc1af5be5eb5f4561170a9280
Edna Gomez Pallares <ednagomezpallares@Ednas-MBP.lan>
2023-11-02 20:25:34 -0400

    Added HTML and JS info

---
+++ index.js
@@ -0,0 +1,10 @@
+const parties= [
+    {id: 1, name: "Emma Lopez", description: "Wedding Reception", date: "2023-09-30T18:00:00.000Z", location: "5015 Fort Hamer Rd, Parrish, FL 34219"},
+    {id: 2, name: "Mia Jones", description: "Birthday Party", date: "2023-09-30T15:00:00.000Z", location: "1339 E Fletcher Ave, Tampa, FL 33612"},
+    {id: 3, name: "Liam Parker", description: "Halloween Party", date: "2023-10-31T19:30:00.000Z", location: "6152 126th Ave N #504, Largo, FL 33773"},
+    {id: 4, name: "Jack Miller", description: "Pool Party", date: "2023-11-04T16:00:00.000Z", location: "1701 Collins Ave, Miami Beach, FL 33139"},
+    {id: 5, name: "Luca Adams", description: "Wedding Reception", date: "2023-11-11T18:00:00.000Z", location: "5015 Fort Hamer Rd, Parrish, FL 34219"},
+    {id: 6, name: "Sara Lee", description: "Fundraiser", date: "2023-11-18T11:00:00.000Z", location: "6463 W Hillsborough Ave, Tampa, FL 33634"},
+    {id: 7, name: "Justin Brooks", description: "Birthday Party", date: "2023-11-25T16:00:00.000Z", location: "1339 E Fletcher Ave, Tampa, FL 33612"},
+    {id: 8, name: "Amy Carter", description: "Graduation Party", date: "2023-12-02T17:30:00.000Z", location: "6152 126th Ave N #504, Largo, FL 33773"},
+]
